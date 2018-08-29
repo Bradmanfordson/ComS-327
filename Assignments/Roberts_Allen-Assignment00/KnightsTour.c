5cad860db4835d509e7bb4f586996406c1050d04
ABradRoberts <bradr555@gmail.com>
2018-08-29 17:35:35 -0500

    Created Makefile and updated so it's more efficient. I have some sort of thing printing but it's not correct.

--- Assignments/Roberts_Allen-Assignment00/KnightsTour.c
+++ Assignments/Roberts_Allen-Assignment00/KnightsTour.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int tour(int x, int y);
+void tour(int x, int y);
 void zeroBoard();
 void printBoard();
 
@@ -9,6 +9,7 @@ int board[5][5];
 int count = 0; 
 
 int main(int argc, char *argv[]){
+
     zeroBoard();
 
     for(int i=0; i<5; i++){
@@ -20,67 +21,33 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-int tour(int x, int y){
+void tour(int x, int y){
     count++;
     board[x][y] = count;  // mark start as visited
 
- /*
-    This is very inefficient. Fix this so it's better
-
-    The idea is to get all possible knight moves and if they are valid and possible given the current space, 
-    make sure we havent moved to that position yet and if we haven't, then move there.
- */
-    for(int r = -2; r < 3; r++){ 
-        for(int c = -2; c < 3; c++){
-            int row = x + r;
-            int col = y + c;
-
-            if( r == 0 || c == 0 || abs(r) == abs(c)){
-                continue; // disregard move because it's an invalid knight move
-            }
-            else {
-                if(row > 0 && row <= 5 && col > 0 && col <= 5){ //for each unmarked square (s) reachable by a knights move from start
-                    if(board[row][col] == 0){ // the tile has not been hit
-                        tour(row, col); // tour(s)
-                    }
-                }
-            }
-        }
-    }
-
-    board[x][y] = 0; // unmark start
-
-
-// Not quite sure what to do from here on.
-/*
-    Not quite sure what to do from here on.
-    My thought process is that I need to check if all of the tiles in the board have been hit
-    and if they have been then print the board and reset the board to all 0's.
-
-    Getting segmentation faults.
-
-
-    int temp = 0;
-    
-    for( int i = 0; i < 5; i++){
-        for( int j = 0; j < 5; j++){
-            if(board[i][j] != 0){
-                temp++;
-            }
-        }
-    }
-
-    // I figure, once the board is full then I need to print the board... 
-
-    if(temp == 25){
+    if ( count == 25 ){
+        count = 0;
         printBoard();
+    } else { // Check all of the valid Knight moves and check that the knight has not moved there yet.
+        if( ( (x + 2) > 0) && ( (x + 2) <= 5) && ( (y + 1) > 0) && ( (y + 1) <= 5) && board[ x + 2 ][ y + 1 ] == 0 ){
+            tour(x + 2, y + 1);
+        } else if ( ( (x + 2) > 0) && ( (x + 2) <= 5) && ( (y - 1) > 0) && ( (y - 1) <= 5 && board[ x + 2 ][ y - 1 ] == 0 ) ){
+            tour(x + 2, y - 1);
+        } else if ( ( (x - 2) > 0) && ( (x - 2) <= 5) && ( (y + 1) > 0) && ( (y + 1) <= 5 && board[ x - 2 ][ y + 1 ] == 0 ) ){
+            tour(x - 2, y + 1);
+        } else if ( ( (x - 2) > 0) && ( (x - 2) <= 5) && ( (y - 1) > 0) && ( (y - 1) <= 5 && board[ x - 2 ][ y - 1 ] == 0 ) ){
+            tour(x - 2, y - 1);
+        } else if ( ( (x + 1) > 0) && ( (x + 1) <= 5) && ( (y + 2) > 0) && ( (y + 2) <= 5 && board[ x + 1 ][ y + 2 ] == 0 ) ){
+            tour(x + 1, y + 2);
+        } else if ( ( (x + 1) > 0) && ( (x + 1) <= 5) && ( (y - 2) > 0) && ( (y - 2) <= 5 && board[ x + 1 ][ y - 2 ] == 0 ) ){
+            tour(x + 1, y - 2);
+        } else if ( ( (x - 1) > 0) && ( (x - 1) <= 5) && ( (y + 2) > 0) && ( (y + 2) <= 5 && board[ x - 1 ][ y + 2 ] == 0 ) ){
+            tour(x - 1, y + 2);
+        } else if ( ( (x - 1) > 0) && ( (x - 1) <= 5) && ( (y - 2) > 0) && ( (y - 2) <= 5 && board[ x - 1 ][ y - 2 ] == 0 ) ){
+            tour(x - 1, y - 2);
+        }
     }
-    printf("\n");
-    count = 0;
-
-*/
-    
-    return 0;
+    board[x][y] = 0; // unmark start
 }
 
 void zeroBoard(){
