e35257a0f0cd2ea44470b40fd529c47a718ae152
ABradRoberts <bradr555@gmail.com>
2018-08-28 20:52:10 -0500

    Tried to work on python algorithm but didnt work so I

--- Assignments/Roberts_Allen-Assignment00/KnightsTour.c
+++ Assignments/Roberts_Allen-Assignment00/KnightsTour.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
 
-int tour(int square);
+int tour(int x, int y);
+
+int board[5][5];
+int count = 0;
 
 int main(int argc, char *argv[]){
-    //for each square (s)
-        // tour(s)
+    for(int i=0; i<5; i++){
+        for(int j = 0; j<5; j++){
+            printf("%2d ", ++count);
+            tour(i, j);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
 
-int tour(int square){
+int tour(int x, int y){
+    board[x][y] = 1;
+
     // mark start as visited
+
     //for each unmarked square (s) reachable by a knights move from start
-        // tour(s)
+        // tour(s
     //unmark start
+    return 0;
 }
 
-// add bookkeeping and print each solution
+
+
