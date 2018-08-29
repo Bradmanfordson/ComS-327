658801cf73ab90520e77f31f3e25293e9c2d6fde
ABradRoberts <bradr555@gmail.com>
2018-08-28 21:19:18 -0500

    Added partial move checker, basically just a nested for-loop to print all combinations of -2, -1, 2, 1.

--- Assignments/Roberts_Allen-Assignment00/KnightsTour.c
+++ Assignments/Roberts_Allen-Assignment00/KnightsTour.c
@@ -9,10 +9,11 @@ int main(int argc, char *argv[]){
     for(int i=0; i<5; i++){
         for(int j = 0; j<5; j++){
             printf("%2d ", ++count);
-            tour(i, j);
+            //tour(i, j);
         }
         printf("\n");
     }
+    tour(1,1);
 
     return 0;
 }
@@ -20,12 +21,28 @@ int main(int argc, char *argv[]){
 int tour(int x, int y){
     board[x][y] = 1;
 
+    for(int r = -2; r < 3; r++){
+        for(int c = -2; c < 3; c++){
+            if( r == 0 || c == 0){
+                continue;
+            }
+            else {
+                printf("( %d , %d ) \n", r, c );
+            }
+        }
+    }
+    
     // mark start as visited
 
     //for each unmarked square (s) reachable by a knights move from start
         // tour(s
     //unmark start
     return 0;
+}
+
+int checkMove(){
+
+    return 0 ;
 }
 
 
