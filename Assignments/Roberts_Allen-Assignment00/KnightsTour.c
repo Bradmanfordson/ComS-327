2265de221fb29c493ef8eb2a5f78ae8cf17498ca
ABradRoberts <bradr555@gmail.com>
2018-08-28 22:22:09 -0500

    Trying to finish the Knights tour but having a tough time actually ending it.

--- Assignments/Roberts_Allen-Assignment00/KnightsTour.c
+++ Assignments/Roberts_Allen-Assignment00/KnightsTour.c
@@ -2,27 +2,28 @@
 #include <stdlib.h>
 
 int tour(int x, int y);
+void printBoard();
+void resetBoard();
 
 int board[5][5];
-int count = 0; // testing purposes
+int count = 1; // testing purposes
 
 int main(int argc, char *argv[]){
+    resetBoard();
+
     for(int i=0; i<5; i++){
         for(int j = 0; j<5; j++){
-            printf("%2d ", ++count); // testing purposes
-            //tour(i, j);
+           printf("Hit\n");
+            tour(i, j);
         }
-        printf("\n");//testing
     }
-    printf("\n"); //testing 
-    //tour(1,1); //testing
-
+    
     return 0;
 }
 
 int tour(int x, int y){
-    board[x][y] = 1;   // mark start as visited
-
+    board[x][y] = count;  // mark start as visited
+    count++;
     for(int r = -2; r < 3; r++){ 
         for(int c = -2; c < 3; c++){
             int row = x + r;
@@ -32,21 +33,53 @@ int tour(int x, int y){
                 continue; // disregard move because it's an invalid knight move
             }
             else {
-                if( row > 0 && row <= 5 && col > 0 && col <= 5 ){ //for each unmarked square (s) reachable by a knights move from start
-                    tour(row, col); // tour(s)
+                if(row > 0 && row <= 5 && col > 0 && col <= 5){ //for each unmarked square (s) reachable by a knights move from start
+                    if(board[row][col] == 0){
+                        tour(row, col); // tour(s)
+                    }
                 }
-                printf("( %d , %d ) \n", r, c ); // testing purposes
             }
         }
     }
-    
+
+
+
     board[x][y] = 0; // unmark start
+
+    int temp = 0;
+    
+    for( int i = 0; i < 5; i++){
+        for( int j = 0; j < 5; j++){
+            if(board[i][j] != 0){
+                temp++;
+            }
+        }
+    }
+    if(temp == 24){
+        printBoard();
+    }
+    printf("\n");
+    count = 0;
+    
     return 0;
 }
 
-int checkMove(){
+void resetBoard(){
+    for(int i = 0; i < 5; i++){
+        for( int j = 0; j < 5; j++){
+            board[i][j] = 0;
+        }
+    }
+}
 
-    return 0 ;
+void printBoard(){
+    for(int i = 0; i < 5; i++){
+        for( int j = 0; j < 5; j++){
+            printf("%d ", board[i][j]);
+        }
+    }
+    printf("\n");
+    resetBoard();
 }
 
 
