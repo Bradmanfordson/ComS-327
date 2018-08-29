35126dc16742828be26059dad44c6848cc0fd896
ABradRoberts <bradr555@gmail.com>
2018-08-28 19:37:55 -0500

    Initialized knights tour with psudocode provided by professor.

---
+++ Assignments/Roberts_Allen-Assignment00/KnightsTour.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int tour(int square);
+
+int main(int argc, char *argv[]){
+    //for each square (s)
+        // tour(s)
+}
+
+int tour(int square){
+    // mark start as visited
+    //for each unmarked square (s) reachable by a knights move from start
+        // tour(s)
+    //unmark start
+}
+
+// add bookkeeping and print each solution
