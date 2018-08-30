d013938a1cd076b602731b889b44d5aeed1e56e2
ABradRoberts <bradr555@gmail.com>
2018-08-29 21:42:57 -0500

    Started DungeonGenerator and added todo list, next step, pseudocode.

---
+++ Assignments/Assignment_1.01/DungeonGenerator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*  TODO
+    1. Create 80x21 boarder
+    2. Create 1 random box in the boarder with rock walls
+    3. add more boxes, dont check for collision yet
+    4. check for collision now and fix
+    5. store box data so we can create the corridors later
+    6. very basic/shitty corridors (dont check for collision)
+    7. Check for collision in corredors and fix any issues
+    8. drink.
+
+*/
