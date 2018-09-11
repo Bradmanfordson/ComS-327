348650d79f250bf10ef08c502c967bbc84120cc9
ABradRoberts <bradr555@gmail.com>
2018-09-10 20:19:39 -0500

    Moved everything around and started save_load.c

---
+++ Assignments/Assignment_1.02/save_load.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+// This is a file used to "test" the features of saving to binary
+// if this file goes as planned then it'll be included in my src
+// and be implemented in the main program.
+
