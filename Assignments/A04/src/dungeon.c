1d39891502c37d41837b155462d57351e73037b6
ABradRoberts <bradr555@gmail.com>
2018-10-01 22:59:27 -0500

    Added regions...

--- Assignments/A04/src/dungeon.c
+++ Assignments/A04/src/dungeon.c
@@ -1,3 +1,4 @@
+#pragma region Headers
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -13,6 +14,10 @@
 
 #include "heap.h"
 #include "dungeon.h"
+
+#pragma endregion
+
+#pragma region Header_Stuff
 
 #define DUMP_HARDNESS_IMAGES 0
 
@@ -95,6 +100,10 @@ typedef struct dungeon
 	uint32_t tmap[DUNGEON_Y][DUNGEON_X];
 } dungeon_t;
 
+#pragma endregion
+
+
+#pragma region Create_Dungeon
 static uint32_t in_room(dungeon_t *d, int16_t y, int16_t x)
 {
 	int i;
@@ -754,65 +763,6 @@ int gen_dungeon(dungeon_t *d)
 	return 0;
 }
 
-void render_dungeon(dungeon_t *d)
-{
-	printf("\nNormal Rendering.\n");
-	pair_t p;
-
-	for (p[dim_y] = 0; p[dim_y] < DUNGEON_Y; p[dim_y]++)
-	{
-		for (p[dim_x] = 0; p[dim_x] < DUNGEON_X; p[dim_x]++)
-		{
-			switch (mappair(p))
-			{
-			case ter_wall:
-			case ter_wall_immutable:
-				if (p[dim_y] == 0 || p[dim_y] == 20)
-				{
-					putchar('-');
-				}
-				else if (p[dim_x] == 0 || p[dim_x] == 79)
-				{
-					putchar('|');
-				}
-				else
-				{
-					putchar(' '); // space
-					int ran = rand() % 255;
-					if (ran == 0)
-					{
-						ran++;
-					}
-					else if (ran == 255)
-					{
-						ran--;
-					}
-				}
-				break;
-			case ter_floor:
-			case ter_floor_room:
-				if (p[dim_x] == d->pc[0] && p[dim_y] == d->pc[1])
-				{
-					putchar('@');
-					break;
-				}
-				else
-				{
-					putchar('.');
-					break;
-				}
-			case ter_floor_hall:
-				putchar('#');
-				break;
-			case ter_debug:
-				putchar('*');
-				fprintf(stderr, "Debug character at %d, %d\n", p[dim_y], p[dim_x]);
-				break;
-			}
-		}
-		putchar('\n');
-	}
-}
 
 void delete_dungeon(dungeon_t *d)
 {
@@ -823,6 +773,11 @@ void init_dungeon(dungeon_t *d)
 {
 	empty_dungeon(d);
 }
+
+#pragma endregion
+
+
+#pragma region Save_Load
 
 int write_dungeon_map(dungeon_t *d, FILE *f)
 {
@@ -1274,6 +1229,70 @@ void usage(char *name)
 	exit(-1);
 }
 
+#pragma endregion
+
+
+void render_dungeon(dungeon_t *d)
+{
+	printf("\nNormal Rendering.\n");
+	pair_t p;
+
+	for (p[dim_y] = 0; p[dim_y] < DUNGEON_Y; p[dim_y]++)
+	{
+		for (p[dim_x] = 0; p[dim_x] < DUNGEON_X; p[dim_x]++)
+		{
+			switch (mappair(p))
+			{
+			case ter_wall:
+			case ter_wall_immutable:
+				if (p[dim_y] == 0 || p[dim_y] == 20)
+				{
+					putchar('-');
+				}
+				else if (p[dim_x] == 0 || p[dim_x] == 79)
+				{
+					putchar('|');
+				}
+				else
+				{
+					putchar(' '); // space
+					int ran = rand() % 255;
+					if (ran == 0)
+					{
+						ran++;
+					}
+					else if (ran == 255)
+					{
+						ran--;
+					}
+				}
+				break;
+			case ter_floor:
+			case ter_floor_room:
+				if (p[dim_x] == d->pc[0] && p[dim_y] == d->pc[1])
+				{
+					putchar('@');
+					break;
+				}
+				else
+				{
+					putchar('.');
+					break;
+				}
+			case ter_floor_hall:
+				putchar('#');
+				break;
+			case ter_debug:
+				putchar('*');
+				fprintf(stderr, "Debug character at %d, %d\n", p[dim_y], p[dim_x]);
+				break;
+			}
+		}
+		putchar('\n');
+	}
+}
+
+
 ///////////////////////////////////////////////////////////////////////////////
 /*                                My Code                                    */
 ///////////////////////////////////////////////////////////////////////////////
@@ -1641,10 +1660,210 @@ void render_dijkstra_ntm(dungeon_t *d)
 			}
     	}
     	putchar('\n');
-}
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 /*                                My Code End                                */
 ///////////////////////////////////////////////////////////////////////////////
 
+int main(int argc, char *argv[])
+{
+#pragma region Vars
+	dungeon_t d;
+	time_t seed;
+	struct timeval tv;
+	uint32_t i;
+	uint32_t do_load, do_save, do_seed, do_image, do_save_seed, do_save_image;
+	uint32_t long_arg;
+	char *save_file;
+	char *load_file;
+	char *pgm_file;
+
+	UNUSED(in_room);
+
+	/* Default behavior: Seed with the time, generate a new dungeon, *
+   * and don't write to disk.                                      */
+	do_load = do_save = do_image = do_save_seed = do_save_image = 0;
+	do_seed = 1;
+	save_file = load_file = NULL;
+
+	/* The project spec rehuires '--load' and '--save'.  It's common  *
+   * to have short and long forms of most switches (assuming you    *
+   * don't run out of letters).  For now, we've got plenty.  Long   *
+   * forms use whole words and take two dashes.  Short forms use an *
+    * abbreviation after a single dash.  We'll add '--rand' (to     *
+   * specify a random seed), which will take an argument of it's    *
+   * own, and we'll add short forms for all three commands, '-l',   *
+   * '-s', and '-r', respectively.  We're also going to allow an    *
+   * optional argument to load to allow us to load non-default save *
+   * files.  No means to save to non-default locations, however.    *
+   * And the final switch, '--image', allows me to create a dungeon *
+   * from a PGM image, so that I was able to create those more      *
+   * interesting test dungeons for you.                             */
+
+#pragma endregion
+
+#pragma region Args
+	if (argc > 1)
+	{
+		for (i = 1, long_arg = 0; i < argc; i++, long_arg = 0)
+		{
+			if (argv[i][0] == '-')
+			{ /* All switches start with a dash */
+				if (argv[i][1] == '-')
+				{
+					argv[i]++;	/* Make the argument have a single dash so we can */
+					long_arg = 1; /* handle long and short args at the same place.  */
+				}
+				switch (argv[i][1])
+				{
+				case 'r':
+					if ((!long_arg && argv[i][2]) ||
+						(long_arg && strcmp(argv[i], "-rand")) ||
+						argc < ++i + 1 /* No more arguments */ ||
+						!sscanf(argv[i], "%lu", &seed) /* Argument is not an integer */)
+					{
+						usage(argv[0]);
+					}
+					do_seed = 0;
+					break;
+				case 'l':
+					if ((!long_arg && argv[i][2]) ||
+						(long_arg && strcmp(argv[i], "-load")))
+					{
+						usage(argv[0]);
+					}
+					do_load = 1;
+					if ((argc > i + 1) && argv[i + 1][0] != '-')
+					{
+						/* There is another argument, and it's not a switch, so *
+             * we'll treat it as a save file and try to load it.    */
+						load_file = argv[++i];
+					}
+					break;
+				case 's':
+					if ((!long_arg && argv[i][2]) ||
+						(long_arg && strcmp(argv[i], "-save")))
+					{
+						usage(argv[0]);
+					}
+					do_save = 1;
+					if ((argc > i + 1) && argv[i + 1][0] != '-')
+					{
+						/* There is another argument, and it's not a switch, so *
+             * we'll save to it.  If it is "seed", we'll save to    *
+	     * <the current seed>.rlg327.  If it is "image", we'll  *
+	     * save to <the current image>.rlg327.                  */
+						if (!strcmp(argv[++i], "seed"))
+						{
+							do_save_seed = 1;
+							do_save_image = 0;
+						}
+						else if (!strcmp(argv[i], "image"))
+						{
+							do_save_image = 1;
+							do_save_seed = 0;
+						}
+						else
+						{
+							save_file = argv[i];
+						}
+					}
+					break;
+				case 'i':
+					if ((!long_arg && argv[i][2]) ||
+						(long_arg && strcmp(argv[i], "-image")))
+					{
+						usage(argv[0]);
+					}
+					do_image = 1;
+					if ((argc > i + 1) && argv[i + 1][0] != '-')
+					{
+						/* There is another argument, and it's not a switch, so *
+             * we'll treat it as a save file and try to load it.    */
+						pgm_file = argv[++i];
+					}
+					break;
+				default:
+					usage(argv[0]);
+				}
+			}
+			else
+			{ /* No dash */
+				usage(argv[0]);
+			}
+		}
+	}
+#pragma endregion 
+	
+	if (do_seed){
+		/* Allows me to generate more than one dungeon *
+     * per second, as opposed to time().           */
+		gettimeofday(&tv, NULL);
+		seed = (tv.tv_usec ^ (tv.tv_sec << 20)) & 0xffffffff;
+	}
+
+	printf("Seed is %ld.\n", seed);
+	srand(seed);
+
+	init_dungeon(&d);
+
+#pragma region do_load
+	if (do_load){
+		read_dungeon(&d, load_file);
+	} else if (do_image){
+		read_pgm(&d, pgm_file);
+	} else {
+		gen_dungeon(&d);
+	}
+#pragma endregion
+
+	/* Set a valid position for the PC */
+	d.pc[dim_x] = d.rooms[0].position[dim_x];
+	d.pc[dim_y] = d.rooms[0].position[dim_y];
+
+	render_dungeon(&d);
+	my_Dijkstra_tm(&d);
+	my_Dijkstra_ntm(&d);
+
+	render_dijkstra_ntm(&d);
+	render_dijkstra_tm(&d);
+	
+#pragma region do_save
+	if (do_save)
+	{
+		if (do_save_seed)
+		{
+			/* 10 bytes for number, plus dot, extention and null terminator. */
+			save_file = malloc(18);
+			sprintf(save_file, "%ld.rlg327", seed);
+		}
+		if (do_save_image)
+		{
+			if (!pgm_file)
+			{
+				fprintf(stderr, "No image file was loaded.  Using default.\n");
+				do_save_image = 0;
+			}
+			else
+			{
+				/* Extension of 3 characters longer than image extension + null. */
+				save_file = malloc(strlen(pgm_file) + 4);
+				strcpy(save_file, pgm_file);
+				strcpy(strchr(save_file, '.') + 1, "rlg327");
+			}
+		}
+		write_dungeon(&d, save_file);
+
+		if (do_save_seed || do_save_image)
+		{
+			free(save_file);
+		}
+	}
+#pragma endregion
+
+	delete_dungeon(&d);
+
+	return 0;
+}
