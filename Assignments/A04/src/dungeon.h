d90505a36b3111ef06191ebcb47e3f8291a4b384
ABradRoberts <bradr555@gmail.com>
2018-10-02 18:35:08 -0500

    Grabbed professors code for 1.03.

--- Assignments/A04/src/dungeon.h
+++ Assignments/A04/src/dungeon.h
@@ -1,187 +1,74 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <endian.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <limits.h>
-#include <sys/time.h>
-#include <errno.h>
+#ifndef DUNGEON_H
+# define DUNGEON_H
 
-/* Very slow seed: 686846853 */
+# include "heap.h"
+# include "macros.h"
+# include "dims.h"
 
-#include "heap.h"
-
-#define DUMP_HARDNESS_IMAGES 0
-
-/* Returns true if random float in [0,1] is less than *
- * numerator/denominator.  Uses only integer math.    */
-#define rand_under(numerator, denominator) \
-	(rand() < ((RAND_MAX / denominator) * numerator))
-
-/* Returns random integer in [min, max]. */
-#define rand_range(min, max) ((rand() % (((max) + 1) - (min))) + (min))
-#define UNUSED(f) ((void)f)
-
-typedef struct corridor_path
-{
-	heap_node_t *hn;
-	uint8_t pos[2];
-	uint8_t from[2];
-	int32_t cost;
-} corridor_path_t;
-
-typedef enum dim
-{
-	dim_x,
-	dim_y,
-	num_dims
-} dim_t;
-
-typedef int16_t pair_t[num_dims];
-
-#define DUNGEON_X 80
-#define DUNGEON_Y 21
-#define MIN_ROOMS 5
-#define MAX_ROOMS 9
-#define ROOM_MIN_X 4
-#define ROOM_MIN_Y 2
-#define ROOM_MAX_X 14
-#define ROOM_MAX_Y 8
-#define SAVE_DIR ".rlg327"
-#define DUNGEON_SAVE_FILE "dungeon"
-#define DUNGEON_SAVE_SEMANTIC "RLG327-F2018"
-#define DUNGEON_SAVE_VERSION 0U
+#define DUNGEON_X              80
+#define DUNGEON_Y              21
+#define MIN_ROOMS              5
+#define MAX_ROOMS              9
+#define ROOM_MIN_X             4
+#define ROOM_MIN_Y             2
+#define ROOM_MAX_X             14
+#define ROOM_MAX_Y             8
+#define SAVE_DIR               ".rlg327"
+#define DUNGEON_SAVE_FILE      "dungeon"
+#define DUNGEON_SAVE_SEMANTIC  "RLG327-F2018"
+#define DUNGEON_SAVE_VERSION   0U
 
 #define mappair(pair) (d->map[pair[dim_y]][pair[dim_x]])
 #define mapxy(x, y) (d->map[y][x])
 #define hardnesspair(pair) (d->hardness[pair[dim_y]][pair[dim_x]])
 #define hardnessxy(x, y) (d->hardness[y][x])
 
-typedef enum __attribute__((__packed__)) terrain_type
-{
-	ter_debug,
-	ter_wall,
-	ter_wall_immutable,
-	ter_floor,
-	ter_floor_room,
-	ter_floor_hall,
+typedef enum __attribute__ ((__packed__)) terrain_type {
+  ter_debug,
+  ter_wall,
+  ter_wall_immutable,
+  ter_floor,
+  ter_floor_room,
+  ter_floor_hall,
 } terrain_type_t;
 
-typedef struct room
-{
-	pair_t position;
-	pair_t size;
+typedef struct room {
+  pair_t position;
+  pair_t size;
 } room_t;
 
-typedef struct dungeon
-{
-	uint32_t num_rooms;
-	room_t *rooms;
-	terrain_type_t map[DUNGEON_Y][DUNGEON_X];
-	/* Since hardness is usually not used, it would be expensive to pull it *
+typedef struct pc {
+  pair_t position;
+} pc_t;
+
+typedef struct dungeon {
+  uint32_t num_rooms;
+  room_t *rooms;
+  terrain_type_t map[DUNGEON_Y][DUNGEON_X];
+  /* Since hardness is usually not used, it would be expensive to pull it *
    * into cache every time we need a map cell, so we store it in a        *
    * parallel array, rather than using a structure to represent the       *
    * cells.  We may want a cell structure later, but from a performanace  *
    * perspective, it would be a bad idea to ever have the map be part of  *
-   * that structure.  Pathfinding will rehuire efficient use of the map,  *
+   * that structure.  Pathfinding will require efficient use of the map,  *
    * and pulling in unnecessary data with each map cell would add a lot   *
    * of overhead to the memory system.                                    */
-	uint8_t hardness[DUNGEON_Y][DUNGEON_X];
-	pair_t pc;
-	uint32_t ntmap[DUNGEON_Y][DUNGEON_X];
-	uint32_t tmap[DUNGEON_Y][DUNGEON_X];
+  uint8_t hardness[DUNGEON_Y][DUNGEON_X];
+  uint8_t pc_distance[DUNGEON_Y][DUNGEON_X];
+  uint8_t pc_tunnel[DUNGEON_Y][DUNGEON_X];
+  pc_t pc;
 } dungeon_t;
 
-static uint32_t in_room(dungeon_t *d, int16_t y, int16_t x);
-
-static uint32_t adjacent_to_room(dungeon_t *d, int16_t y, int16_t x);
-
-static uint32_t is_open_space(dungeon_t *d, int16_t y, int16_t x);
-
-static int32_t corridor_path_cmp(const void *key, const void *with);
-
-static void dijkstra_corridor(dungeon_t *d, pair_t from, pair_t to);
-
-/* This is a cut-and-paste of the above.  The code is modified to  *
- * calculate paths based on inverse hardnesses so that we get a    *
- * high probability of creating at least one cycle in the dungeon. */
-static void dijkstra_corridor_inv(dungeon_t *d, pair_t from, pair_t to);
-
-/* Chooses a random point inside each room and connects them with a *
- * corridor.  Random internal points prevent corridors from exiting *
- * rooms in predictable locations.                                  */
-static int connect_two_rooms(dungeon_t *d, room_t *r1, room_t *r2);
-
-static int create_cycle(dungeon_t *d);
-
-static int connect_rooms(dungeon_t *d);
-
-int gaussian[5][5] = {
-	{1, 4, 7, 4, 1},
-	{4, 16, 26, 16, 4},
-	{7, 26, 41, 26, 7},
-	{4, 16, 26, 16, 4},
-	{1, 4, 7, 4, 1}};
-
-typedef struct hueue_node
-{
-	int x, y;
-	struct hueue_node *next;
-} hueue_node_t;
-
-static int smooth_hardness(dungeon_t *d);
-static int empty_dungeon(dungeon_t *d);
-
-static int place_rooms(dungeon_t *d);
-
-static int make_rooms(dungeon_t *d);
-
-int gen_dungeon(dungeon_t *d);
-
-void render_dungeon(dungeon_t *d);
-
-void delete_dungeon(dungeon_t *d);
-
 void init_dungeon(dungeon_t *d);
-
-int write_dungeon_map(dungeon_t *d, FILE *f);
-
-int write_rooms(dungeon_t *d, FILE *f);
-
-uint32_t calculate_dungeon_size(dungeon_t *d);
-
-int makedirectory(char *dir);
-
+void delete_dungeon(dungeon_t *d);
+int gen_dungeon(dungeon_t *d);
+void render_dungeon(dungeon_t *d);
 int write_dungeon(dungeon_t *d, char *file);
-
-int read_dungeon_map(dungeon_t *d, FILE *f);
-
-int read_rooms(dungeon_t *d, FILE *f);
-int calculate_num_rooms(uint32_t dungeon_bytes);
-
 int read_dungeon(dungeon_t *d, char *file);
-
 int read_pgm(dungeon_t *d, char *pgm);
+void render_distance_map(dungeon_t *d);
+void render_tunnel_distance_map(dungeon_t *d);
+void render_hardness_map(dungeon_t *d);
+void render_movement_cost_map(dungeon_t *d);
 
-void usage(char *name);
-
-///////////////////////////////////////////////////////////////////////////////
-/*                                My Code                                    */
-///////////////////////////////////////////////////////////////////////////////
-
-
-#define distancepair(pair) (d->hardness[pair[dim_y]][pair[dim_x]]/85 +1)
-
-void my_Dijkstra_tm(dungeon_t *d);
-void my_Dijkstra_ntm(dungeon_t *d);
-
-void render_dijkstra_tm(dungeon_t *d);
-
-void render_dijkstra_ntm(dungeon_t *d);
-
-///////////////////////////////////////////////////////////////////////////////
-/*                                My Code End                                */
-///////////////////////////////////////////////////////////////////////////////
-
+#endif
--- Assignments/A04/src/heap.c
+++ Assignments/A04/src/heap.c
@@ -3,16 +3,87 @@
 
 #undef min
 
+// General structure of the heap: The heap is made of heap_nodes(the struct below). Each 
+// node has 4 pointers to other nodes, this is how everything is connected. These 4 pointers
+// create two seperate lists. The "next" list and the "child" list. The "child" list is simple
+// and just a list going from the beginning(min) to the last child. This list is traversed
+// with [*parent] and [*child]. The "next" list is a little more complicated and contains 
+// all of the values that have the same parent. One key feature of this list is that it is
+// a loop aning that if you iterate with [*next] you will reach the current node again. This
+// list can be iterated with [*next] and [*prev].
+//
+// Diagram of heap:
+// Note: The items in brackets are nodes that the current node has pointers to.
+//
+//  [*parent]
+//    |
+//  [*prev] <- {curr_node} -> [*next]
+//      |           |             |
+//      |           |           next.child -> next.child.next
+//      |           |
+//      |         [*child] -> child.next -> child.next.next
+//      |             |                               |
+//      |             |                     child.next.next.child
+//      |             |
+//      |          child.child
+//      |
+//     prev.child -> prev.child-next
+//
+// Diagram of a "child" list:
+//
+//      parent.parent
+//            |
+//        [*parent]
+//            |
+//       {curr_node}
+//            |
+//        [*child]
+//            |
+//       child.child
+//
+// Diagram of a "next" list:
+// Note: since the "next" list is a loop. next->next is connected to prev->prev
+//
+// prev.prev <- [*prev] <- {curr_node} -> [*next] -> next.next
+//     ^                                                   |
+//     |                                                   |
+//     ----------------------<next>-------------------------
+
+// Important struct only known to the heap functions.
 struct heap_node {
+
+  // The next node with the same parent.
   heap_node_t *next;
+
+  // The previous node with the same parent.
   heap_node_t *prev;
+
+  // The parent to this node. Min shouldn't have a child.
   heap_node_t *parent;
+
+  // Child to the node. This only stores one of the children. To get to the other you
+  // must iterate through *next.
   heap_node_t *child;
+
+  // The main data stored in this node.
   void *datum;
+
+  // The amount of children the node has.
   uint32_t degree;
+
+  // Used with heap decrease key.
   uint32_t mark;
 };
 
+// Macro(like a function): splice_heap_node_lists
+// If both n1 and n2 exist, connect n1 and n2 as [next] and [prev]. Then connect n1->next
+// to n2->prev.
+// from:
+// [n1] -- n1->next
+// n2->prev -- [n2]
+// to:
+// [n1] -- [n2]
+// n2->prev -- n1->next
 #define splice_heap_node_lists(n1, n2) ({ \
   if ((n1) && (n2)) {                     \
     (n1)->next->prev = (n2)->prev;        \
@@ -22,6 +93,8 @@ struct heap_node {
   }                                       \
 })
 
+// Macro: insert_heap_node_in_list
+// Inserts node n into the list l is in. It is inserted before node l.
 #define insert_heap_node_in_list(n, l) ({ \
   (n)->next = (l);                        \
   (n)->prev = (l)->prev;                  \
@@ -29,6 +102,9 @@ struct heap_node {
   (l)->prev = (n);                        \
 })
 
+// Macro: remove_heap_node_from_list
+// Removes the given node from the heap list. It does this by connecting the [next] and [prev]
+// nodes together.
 #define remove_heap_node_from_list(n) ({ \
   (n)->next->prev = (n)->prev;           \
   (n)->prev->next = (n)->next;           \
@@ -83,16 +159,19 @@ void print_heap_node_list(heap_node_t *n)
   printf("\n");
 }
 
+// Inits heap
 void heap_init(heap_t *h,
                int32_t (*compare)(const void *key, const void *with),
                void (*datum_delete)(void *))
 {
+  // Sets compare function and deatum_delete function to the given values.
   h->min = NULL;
   h->size = 0;
   h->compare = compare;
   h->datum_delete = datum_delete;
 }
 
+// Goes through heap and deletes hn from the heap.
 void heap_node_delete(heap_t *h, heap_node_t *hn)
 {
   heap_node_t *next;
@@ -101,8 +180,9 @@ void heap_node_delete(heap_t *h, heap_node_t *hn)
   while (hn) {
     if (hn->child) {
       heap_node_delete(h, hn->child);
-    } 
+    }
     next = hn->next;
+    // If a datum_delete was given, use it.
     if (h->datum_delete) {
       h->datum_delete(hn->datum);
     }
@@ -111,6 +191,7 @@ void heap_node_delete(heap_t *h, heap_node_t *hn)
   }
 }
 
+// deletes entire heap. By setting everthing to null.
 void heap_delete(heap_t *h)
 {
   if (h->min) {
@@ -122,18 +203,24 @@ void heap_delete(heap_t *h)
   h->datum_delete = NULL;
 }
 
+// Inserts value into the heap. It does this by adding v into the the upper level list.
 heap_node_t *heap_insert(heap_t *h, void *v)
 {
+  // Init new heap node with given values
   heap_node_t *n;
 
   n = calloc(1, sizeof (*n));
   n->datum = v;
 
+  // If min exists add it to the top level list.
   if (h->min) {
     insert_heap_node_in_list(n, h->min);
   } else {
+    // If min doesn't exists that means this is the first value so set it up like one.
     n->next = n->prev = n;
   }
+
+  // Set n to min if it is the smallest value.
   if (!h->min || (h->compare(v, h->min->datum) < 0)) {
     h->min = n;
   }
@@ -147,12 +234,16 @@ void *heap_peek_min(heap_t *h)
   return h->min ? h->min->datum : NULL;
 }
 
+// Adds {node} as a child of {root} 
 static void heap_link(heap_t *h, heap_node_t *node, heap_node_t *root)
 {
   /*  remove_heap_node_from_list(node);*/
+
+  // If the {root} node has a [child], insert {node} into the end of the [child] list.
   if (root->child) {
     insert_heap_node_in_list(node, root->child);
   } else {
+    // If root has no [child] add {node} as the child of root.
     root->child = node;
     node->next = node->prev = node;
   }
@@ -161,6 +252,7 @@ static void heap_link(heap_t *h, heap_node_t *node, heap_node_t *root)
   node->mark = 0;
 }
 
+// Consolidates the heap.
 static void heap_consolidate(heap_t *h)
 {
   uint32_t i;
@@ -169,32 +261,58 @@ static void heap_consolidate(heap_t *h)
                        * to the limit of a 64-bit address space,  *
                        * and much faster than any lg calculation. */
 
+  // Inits array a with all 0s
   memset(a, 0, sizeof (a));
 
+  // Changes min's [prev] node to point to nothing. This essentially cuts the "next" list
+  // so that it doesn't loop
   h->min->prev->next = NULL;
 
+  // This for loop essintaily makes the heap into a more tree like structure. It gives
+  // the heap more depth by iterating though the top list and fusing together nodes with
+  // the same amount of children.
   for (x = n = h->min; n; x = n) {
+
+    // set n to the next value in the list, this is just to store it for the next iteration.
     n = n->next;
 
+    // If a previous node has the same [degree] as the current node, make the greater one a 
+    // child of the small node. Then check if this new node has the same [degree] of a previous 
+    // node, repeat. Reminder, the entire structure is a heap and the [degree] is the amount of
+    // children the node has.
     while (a[x->degree]) {
       y = a[x->degree];
+
+      // If {x}'s value is greater, swap the values in {x} and {y}. This means that {x} contains 
+      // the node with the smallest value.
       if (h->compare(x->datum, y->datum) > 0) {
         swap(x, y);
       }
+
+      // Set the value at the current list to null
       a[x->degree] = NULL;
+
+      // Make {y} a child of {x}.
       heap_link(h, y, x);
     }
+
+    // Put current node into the list at the location of the amount of children it has.
     a[x->degree] = x;
   }
 
+  // After the for loop above, you will be left with a few roots in the "a" list. One of
+  // these is the min node. Go through the nodes and figure out which one is the root.
+  // Add the others to the min list.
   for (h->min = NULL, i = 0; i < 64; i++) {
     if (a[i]) {
       if (h->min) {
+        // If the min node exists, check if the current node is smaller. If it is swap them.
         insert_heap_node_in_list(a[i], h->min);
         if (h->compare(a[i]->datum, h->min->datum) < 0) {
           h->min = a[i];
         }
       } else {
+        // If no min node is found yet, use this as the min node.
         h->min = a[i];
         a[i]->next = a[i]->prev = a[i];
       }
@@ -202,6 +320,7 @@ static void heap_consolidate(heap_t *h)
   }
 }
 
+// removes the minimum node from the heap. A lot of other fancy stuff happens here as well.
 void *heap_remove_min(heap_t *h)
 {
   void *v;
@@ -209,25 +328,51 @@ void *heap_remove_min(heap_t *h)
 
   v = NULL;
 
+  // Something to note. When the heap is first created
+
+  // if min exits
   if (h->min) {
+    // set v to min's value
     v = h->min->datum;
+
+    // if size of heap is zero that means we have no more heap, so free min and set min to null
     if (h->size == 1) {
       free(h->min);
       h->min = NULL;
     } else {
+
+      // This basiclly just goes through min's children and sets their parent pointer to
+      // null.
       if ((n = h->min->child)) {
         for (; n->parent; n = n->next) {
           n->parent = NULL;
         }
       }
 
+      // Sets the [child] of min's [next]. This brings the rest min's child nodes
+      // up a level to the top level and appends the min's original [next] list to the end.
+      //
+      // Example:
+      // NOTE: [prev_1] is connected to [next_1] and [prev_2] is connected to [prev_2]. This is
+      // because the [next] lists are a loop.
+      // Before:
+      // [prev_1] <- {min} -> [next_1]
+      //               |
+      // [prev_2] <- [child] -> [next_2]
+      //
+      // After:
+      // [prev_1] <- {min} -> [child] -> [next_2] -> [prev_2] -> [next_1]
+      //
+      // TLDR: prepends the [child] list to the [next] list.
       splice_heap_node_lists(h->min, h->min->child);
 
+      // Set n from the heap, make the child the new min.
       n = h->min;
       remove_heap_node_from_list(n);
       h->min = n->next;
       free(n);
 
+      // TLDR: Makes the heap more heap like.
       heap_consolidate(h);
     }
 
@@ -266,20 +411,29 @@ int heap_combine(heap_t *h, heap_t *h1, heap_t *h2)
   return 0;
 }
 
+// Removes n from p's child list and adds it to the upper level list(min's list).
 static void heap_cut(heap_t *h, heap_node_t *n, heap_node_t *p)
 {
+  // If decrease the amount of children "p" has and if p has no more children, set child to null.
   if (!--p->degree) {
     p->child = NULL;
   }
+
+  // If the direct child of "p" is "n" set p's child to the next child.
   if (p->child == n) {
     p->child = p->child->next;
   }
+
+  // Removes "n" from p's child list.
   remove_heap_node_from_list(n);
   n->parent = NULL;
   n->mark = 0;
+
+  // Inserts "n" into the upper level list.
   insert_heap_node_in_list(n, h->min);
 }
 
+// heap cuts node n up until it doesn't have any parents left.
 static void heap_cascading_cut(heap_t *h, heap_node_t *n)
 {
   heap_node_t *p;
@@ -308,6 +462,8 @@ int heap_decrease_key(heap_t *h, heap_node_t *n, void *v)
   return heap_decrease_key_no_replace(h, n);
 }
 
+// Updates the heap after n's value was decreased. Use this after you decrease the
+// value that is pointed to by n.
 int heap_decrease_key_no_replace(heap_t *h, heap_node_t *n)
 {
   /* No tests that the value hasn't actually increased.  Change *
@@ -317,12 +473,19 @@ int heap_decrease_key_no_replace(heap_t *h, heap_node_t *n)
 
   heap_node_t *p;
 
+  // Get the parent of the given node
   p = n->parent;
 
+  // If the parent to n exists and is larger than n, ...
   if (p && (h->compare(n->datum, p->datum) < 0)) {
+
+    // Moves the node n from p's child list and adds to the upper level list.
     heap_cut(h, n, p);
+    // Pushes node n up until it doesn't have any more parents.
     heap_cascading_cut(h, p);
   }
+
+  // If node n is smaller than the current min, make n min.
   if (h->compare(n->datum, h->min->datum) < 0) {
     h->min = n;
   }
@@ -374,7 +537,7 @@ int main(int argc, char *argv[])
 
   print_heap(&h, print_int);
   printf("------------------------------------\n");
-  
+
   heap_remove_min(&h);
   keys[0] = malloc(sizeof (*keys[0]));
   *keys[0] = 0;
